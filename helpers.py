5896f7602d5b80d6e2df07290eddcdb7b6d92ca2
ozmartins <50338986+ozmartins@users.noreply.github.com>
2020-08-20 13:21:08 -0300

    .

--- helpers.py
+++ helpers.py
@@ -11,5 +11,4 @@ def apology(message, code=400):
                          ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
             s = s.replace(old, new)
         return s
-        
     return render_template("apology.html", top=code, bottom=escape(message)), code
