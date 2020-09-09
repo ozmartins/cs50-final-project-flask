5ee2ca990fe38513c90561ef85de2356472e2717
ozmartins <50338986+ozmartins@users.noreply.github.com>
2020-09-09 12:44:08 -0300

    .

--- application.py
+++ application.py
@@ -1,13 +1,15 @@
 import os
+import requests
+
 from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
-import requests
 from helpers import apology
 from market_data import get_market_indicators, get_market_news
+
 
 # Configure application
 app = Flask(__name__)
