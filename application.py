fb44850fbe0cc9b00a99a6ba6c3006a3e110da8f
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2020-12-08 13:11:42 -0300

    Update application.py

--- application.py
+++ application.py
@@ -119,4 +119,4 @@ for code in default_exceptions:
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port)    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
