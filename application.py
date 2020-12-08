02c36fb88067b620aeaf6ceb2d7aa5202d467c02
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2020-12-08 12:48:31 -0300

    heorku

--- application.py
+++ application.py
@@ -118,4 +118,5 @@ for code in default_exceptions:
     app.errorhandler(code)(errorhandler)
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port)
